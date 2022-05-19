ee445252979d26f8677f6667c01e4106dabbbba2
Lyle Buendia <lylebuendia@mail.fresnostate.edu>
2022-05-19 04:09:25 -0700

    lab 3 finished

---
+++ Lab 3/class_grade.py
@@ -0,0 +1,23 @@
+grades = []
+
+grade = input("Enter the 1st grade: ")
+grades.append(float(grade))
+
+grade = input("Enter the 2nd grade: ")
+grades.append(float(grade))
+
+grade = input("Enter the 3rd grade: ")
+grades.append(float(grade))
+
+grade = input("Enter the 4th grade: ")
+grades.append(float(grade))
+
+grade = input("Enter the 5th grade: ")
+grades.append(float(grade))
+
+grades.sort()
+grades = grades[2:]
+grades = sum(grades)
+result = grades / 3
+
+print("Average grade {0:2f}%".format(result))
